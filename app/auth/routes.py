c09025608868a83bf00240ef479a5a65009108bc
Nikita Vanda <nikitavanda149@proton.me>
2025-01-11 22:17:12 +0200

    add login and password hashing

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,6 +1,6 @@
-import json
-
+import bcrypt
 from flask import request, jsonify
+from flask_jwt_extended import create_access_token
 
 from app.auth import bp
 from app.models import User
@@ -20,9 +20,25 @@ def register():
     if User.query.filter_by(username=username).first():
         return jsonify({"error": "Username already exists"}), 400
 
-    new_user = User(username=username, password=password)
-    #TODO: Add password hashing
+    pw_hash = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    new_user = User(username=username, password=pw_hash)
+
     db.session.add(new_user)
     db.session.commit()
 
     return jsonify({"id": new_user.id, "username": new_user.username}), 201
+
+
+@bp.route('/login/', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    user = User.query.filter_by(username=username).first()
+
+    if user and bcrypt.checkpw(password.encode('utf-8'), user.password):
+        access_token = create_access_token(identity=user.id)
+        return jsonify({'message': 'Login Success', 'access_token': access_token})
+    else:
+        return jsonify({'message': 'Login Failed'}), 401
